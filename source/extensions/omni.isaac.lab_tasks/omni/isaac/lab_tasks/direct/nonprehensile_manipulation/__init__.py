e7e7ed2d7aa825a9310b4f443a766c0b5005bf89
SB <sbisthebest@snu.ac.kr>
2024-12-02 15:54:51 +0900

    SKRL, SB3 added

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/nonprehensile_manipulation/__init__.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/nonprehensile_manipulation/__init__.py
@@ -14,6 +14,8 @@ gym.register(
     disable_env_checker=True,
     kwargs={
         "env_cfg_entry_point" : f"{__name__}.nonprehensile_manipulation_env:NonPrehensileManipulationEnvCfg",
-        "rl_games_cfg_entry_point" : f"{agents.__name__}:rl_games_ppo_cfg.yaml"
+        "rl_games_cfg_entry_point" : f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+        "skrl_cfg_entry_point" : f"{agents.__name__}:skrl_ppo_cfg.yaml",
+        "sb3_cfg_entry_point" : f"{agents.__name__}:sb3_ppo_cfg.yaml",
     }
 )
