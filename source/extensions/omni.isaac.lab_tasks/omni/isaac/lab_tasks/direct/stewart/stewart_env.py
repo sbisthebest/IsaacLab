32872f174dea7816a718052538e5e983dec45e4b
SB <sbisthebest@snu.ac.kr>
2024-12-03 22:15:44 +0900

    Nice Setup..?

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/stewart/stewart_env.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/stewart/stewart_env.py
@@ -11,7 +11,7 @@ from omni.isaac.lab.sim.spawners.from_files import GroundPlaneCfg, spawn_ground_
 from omni.isaac.lab_assets import ISAACLAB_ASSETS_DATA_DIR
 from omni.isaac.lab.utils.assets import ISAAC_NUCLEUS_DIR
 from omni.isaac.lab.utils import configclass
-from omni.isaac.lab.utils.math import quat_apply, quat_inv
+from omni.isaac.lab.utils.math import quat_apply, quat_inv, matrix_from_quat
 
 @configclass
 class StewartManipulationEnvCfg(DirectRLEnvCfg):
@@ -19,7 +19,7 @@ class StewartManipulationEnvCfg(DirectRLEnvCfg):
     episode_length_s = 10.
     decimation = 2
     action_space = 3
-    observation_space = 9
+    observation_space = 18
     state_space = 0
 
     # simulation config
@@ -60,8 +60,8 @@ class StewartManipulationEnvCfg(DirectRLEnvCfg):
             ),
             articulation_props = sim_utils.ArticulationRootPropertiesCfg(
                 enabled_self_collisions=False,
-                solver_position_iteration_count = 6,
-                solver_velocity_iteration_count = 3
+                solver_position_iteration_count = 12,
+                solver_velocity_iteration_count = 6
             ),
         ),
         init_state = ArticulationCfg.InitialStateCfg(
@@ -154,7 +154,6 @@ class StewartManipulationEnv(DirectRLEnv):
         self.rel_vel = torch.zeros((self.num_envs, 2)).to(self.sim.device)
         self.ball_to_targets = torch.zeros((self.num_envs, 2)).to(self.sim.device)
         self.robot_dof_targets = torch.zeros((self.num_envs, 3)).to(self.sim.device)
-        self.latent_height = torch.zeros((self.num_envs, 1)).to(self.sim.device)
 
         self.succeed_env_ids = torch.zeros((self.num_envs), dtype=torch.bool).to(self.sim.device)
         self.terminated_env_ids = torch.zeros((self.num_envs), dtype=torch.bool).to(self.sim.device)
@@ -191,7 +190,8 @@ class StewartManipulationEnv(DirectRLEnv):
         self.robot.set_joint_position_target(self.robot_dof_targets, joint_ids=[9, 10, 11])
 
     def _get_observations(self):
-        # breakpoint()
+        self.plane_rot_mat = matrix_from_quat(self.plane_quaternion).reshape(-1, 9)
+
         obs = torch.cat(
             (
                 self.ball_to_targets,
@@ -199,6 +199,7 @@ class StewartManipulationEnv(DirectRLEnv):
                 self.rel_vel,
                 self.ball_targets,
                 self.robot_dof_targets,
+                self.plane_rot_mat
             ),
             dim=-1,
         )
@@ -218,7 +219,6 @@ class StewartManipulationEnv(DirectRLEnv):
             self.ball_to_targets[:],
             self.rel_pos[:],
             self.rel_vel[:],
-            self.latent_height[:],
             self.succeed_env_ids[:],
             self.terminated_env_ids[:],
         )=compute_intermediate_values(
@@ -232,11 +232,11 @@ class StewartManipulationEnv(DirectRLEnv):
             self.cfg.lower_leg_lenth,
         )
 
-        self.success_env_count += torch.sum(self.succeed_env_ids)
-        self.failed_env_count += torch.sum(self.terminated_env_ids)
+        # self.success_env_count += torch.sum(self.succeed_env_ids)
+        # self.failed_env_count += torch.sum(self.terminated_env_ids)
 
-        if torch.sum(self.succeed_env_ids) > 0:
-            print(f"success ratio : {self.success_env_count / (self.success_env_count + self.failed_env_count) * 100}%")
+        # if torch.sum(self.succeed_env_ids) > 0:
+        #     print(f"success ratio : {self.success_env_count / (self.success_env_count + self.failed_env_count) * 100}%")
 
         total_reward = compute_rewards(self.ball_to_targets,
                                        self.rel_vel,
@@ -265,25 +265,33 @@ class StewartManipulationEnv(DirectRLEnv):
 
         super()._reset_idx(env_ids)
 
+        # initialize ball targets
+        self.ball_targets[env_ids, :] = self._reset_target(env_ids)
+
+        # initialize stewart
         init_joint_pos, init_stewart_pos, init_ball_pos = self._reset_stewart(env_ids)
 
-        init_stewart_pos[:, 0:3] += self.env_origins[env_ids]
+        init_mask = torch.norm(init_ball_pos[:, 0:2] - self.ball_targets[env_ids, :], dim=-1, keepdim=True) < 2e-2
+
+        self.ball_targets[env_ids, :] = torch.where(init_mask, torch.zeros_like(self.ball_targets[env_ids, :]), self.ball_targets[env_ids, :])
+        init_ball_pos[:, 0:2] = torch.where(init_mask, 0.4 * self.cfg.plane_length * torch.ones_like(init_ball_pos[:, 0:2]), init_ball_pos[:, 0:2])
+
         init_stewart_vel = torch.zeros((len(env_ids), 6), device=self.sim.device)
         init_joint_vel = torch.zeros_like(init_joint_pos)
+
+        # initialize ball position
+        init_ball_vel = torch.zeros((len(env_ids), 6), device=self.sim.device)
+
+        # translate to local env
+        init_stewart_pos[:, 0:3] += self.env_origins[env_ids]
+        init_ball_pos[:, 0:3] += self.env_origins[env_ids]
 
         self.robot.set_joint_position_target(init_joint_pos, env_ids=env_ids)
         self.robot.write_joint_state_to_sim(init_joint_pos, init_joint_vel, env_ids=env_ids)
         self.robot.write_root_pose_to_sim(init_stewart_pos, env_ids=env_ids)
         self.robot.write_root_velocity_to_sim(init_stewart_vel, env_ids=env_ids)
-
-        # init_ball_pos = self.ball.data.default_root_state
-        init_ball_pos[:, 0:3] += self.env_origins[env_ids]
-        init_ball_vel = torch.zeros((len(env_ids), 6), device=self.sim.device)
-
         self.ball.write_root_pose_to_sim(init_ball_pos, env_ids=env_ids)
         self.ball.write_root_velocity_to_sim(init_ball_vel, env_ids=env_ids)
-
-        self.ball_targets[env_ids, :] = self._reset_target(env_ids)
 
     def _reset_stewart(self, env_ids):
         upper_joint = torch.rand((len(env_ids), 1)).to(self.sim.device)
@@ -307,7 +315,7 @@ class StewartManipulationEnv(DirectRLEnv):
                                          torch.zeros(len(env_ids), 3, device=self.sim.device)))
 
         init_ball_pos = torch.hstack((0.8 * self.cfg.plane_length * torch.rand((len(env_ids), 2), device=self.sim.device) - 0.4 * self.cfg.plane_length,
-                                      plane_height + 0.1,
+                                      plane_height + self.cfg.plane_height/2,
                                       torch.ones(len(env_ids), 1, device=self.sim.device),
                                       torch.zeros(len(env_ids), 3, device=self.sim.device)))
 
@@ -348,11 +356,8 @@ def compute_intermediate_values(
     _rel_vel = quat_apply(quat_inv(plane_quat), ball_vel_projected)
     rel_vel = _rel_vel[..., 0:2]
 
-    # Calculate latent height
-    latent_height = lower_leg_length * torch.sum(torch.cos(joint_angle), dim=-1, keepdim=True)/3
-
     # Calculate succeed enviornments
-    succeed_env_ids = (torch.norm(ball_to_targets, dim=-1) < 1e-2) & (torch.norm(rel_vel, dim=-1) < 1e-3)
+    succeed_env_ids = (torch.norm(ball_to_targets, dim=-1) < 2e-2) & (torch.norm(rel_vel, dim=-1) < 1e-3)
 
     # Calculate terminated environments
     terminated_env_ids = torch.max(torch.abs(rel_pos), dim=-1)[0] >= plane_length/2
@@ -361,7 +366,6 @@ def compute_intermediate_values(
         ball_to_targets,
         rel_pos,
         rel_vel,
-        latent_height,
         succeed_env_ids,
         terminated_env_ids,
     )
@@ -376,16 +380,16 @@ def compute_rewards(
     max_episode_length : float,
 ):
 
-    pos_reward = 1./(1. + 100 * torch.norm(ball_to_targets, dim=-1))
-    vel_reward = 1./(1. + 100 * torch.norm(rel_vel, dim=-1))
+    pos_reward = 1./(1. + 10 * torch.norm(ball_to_targets, dim=-1))
+    vel_reward = 1./(1. + 1 * torch.norm(rel_vel, dim=-1))
 
     # alive_reward = torch.ones_like(pos_reward)
 
-    total_reward = pos_reward + vel_reward
+    total_reward = pos_reward * vel_reward
 
     total_reward = torch.where(succeed_env_ids, 10 * torch.ones_like(pos_reward), total_reward)
 
-    total_reward = torch.where(terminated_env_ids, -5 * torch.ones_like(total_reward), total_reward)
+    total_reward = torch.where(terminated_env_ids, -100 * torch.ones_like(total_reward), total_reward)
     # total_reward = torch.where(episode_length_buf >= max_episode_length - 1, -1 * torch.ones_like(total_reward), total_reward)
 
     return total_reward
